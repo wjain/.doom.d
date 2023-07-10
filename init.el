4c39e1a29573c723babc85b12ebe2eecec647b7e
wenjian <jain_y@126.com>
2023-07-10 16:06:53 +0800

    Add conda & lsp for python

--- init.el
+++ init.el
@@ -176,7 +176,8 @@
        (php +lsp)              ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +conda            ; beautiful is better than ugly
+               +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
